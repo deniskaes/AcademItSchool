01063aa479eede240653a053fccc6651facc251e
esaulov <deniskaes@gmail.com>
2018-03-01 17:42:49 +0700

    mod

--- src/Lesson5/Deposit.java
+++ src/Lesson5/Deposit.java
@@ -6,6 +6,8 @@ public class Deposit {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int capitalizationPeriod = 1;
+
         System.out.println("Введите сумму денежных средств:");
         int initialAmount = scanner.nextInt();
 
@@ -13,12 +15,20 @@ public class Deposit {
         int month = scanner.nextInt();
 
         System.out.println("Введи ставку:");
-        double rate  = scanner.nextDouble();
+        double rate = scanner.nextDouble();
 
-        double profit = initialAmount * rate * month / 1200;
+        double profit = initialAmount * rate * month / 12 / 100;
         double payout = profit + initialAmount;
 
         System.out.println("Выплата в конце срока =" + payout);
         System.out.println("Прибыль = " + profit);
+
+        int profitPeriods = month / capitalizationPeriod;
+
+        double payoutWithCapitalization = initialAmount * (Math.pow((1 + rate * capitalizationPeriod / 12 / 100), profitPeriods));
+        double profitWithCapitalization = payoutWithCapitalization - initialAmount;
+
+        System.out.println("Выплата с капитализацией в конце срока =" + payoutWithCapitalization);
+        System.out.println("Прибыль с капитализацией= " + profitWithCapitalization);
     }
 }
