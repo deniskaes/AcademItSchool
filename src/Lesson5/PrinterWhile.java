71916243265422d3e2274588dcbc727031a16c3e
Denis <deniskaes@gmail.com>
2018-03-01 20:46:41 +0700

    mod

--- src/Lesson5/PrinterWhile.java
+++ src/Lesson5/PrinterWhile.java
@@ -4,7 +4,7 @@ public class PrinterWhile {
     public static void main(String[] args) {
         int i = 0;
         while (i <= 100) {
-            if (i != 5 && i % 3 != 0 && (i <= 60 || i >= 80)){
+            if (i != 5 && i % 3 != 0 && (i <= 60 || i >= 80)) {
                 System.out.println(i);
             }
             i++;
