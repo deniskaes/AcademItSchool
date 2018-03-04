161cdb98661bc1062314b4244eb942867a17d6f1
Denis <deniskaes@gmail.com>
2018-03-04 11:33:21 +0700

    mod

---
+++ src/Lesson6/OrderValue.java
@@ -0,0 +1,32 @@
+package Lesson6;
+
+import java.util.Scanner;
+
+public class OrderValue {
+    public static double getValue(int firstType, int secondType, double costFistType, double costSecondType) {
+        double rate;
+        double costBasket = firstType * costFistType + secondType * costSecondType;
+
+        if (costBasket >= 1000 && firstType + secondType >= 10) {
+            rate = 0.90;
+        } else if (firstType + secondType >= 10 || costBasket >= 1000) {
+            rate = 0.95;
+        } else rate = 1;
+
+        return costBasket * rate;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите количество и цену первого товара:");
+        int fistType = scanner.nextInt();
+        double costFirstType = scanner.nextDouble();
+
+        System.out.println("Введите количество и цену второго товара:");
+        int secondType = scanner.nextInt();
+        double costSecondType = scanner.nextDouble();
+
+        System.out.println(getValue(fistType, secondType, costFirstType, costSecondType));
+    }
+}
