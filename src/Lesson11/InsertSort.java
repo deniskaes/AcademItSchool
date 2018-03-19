cb4c9a651168385d64610f6de7a3dd2dfeed7808
Denis <deniskaes@gmail.com>
2018-03-19 07:50:07 +0700

    mod

--- src/Lesson11/InsertSort.java
+++ src/Lesson11/InsertSort.java
@@ -11,7 +11,7 @@ public class InsertSort {
             if (array[i - 1] > array[i]) {
                 int temp = array[i];
                 int j = i-1;
-                while (j>0 && temp < array[j])
+                while (j>0 && temp < array[j]){}
 //                while (j > 0 && array[j - 1] > array[j]) {
 //                    int temp = array[j - 1];
 //                    array[j - 1] = array[j];
