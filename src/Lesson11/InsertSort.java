3673ea919575fab48153d16a62efb7763be223d8
esaulov <deniskaes@gmail.com>
2018-03-20 10:09:42 +0700

    mod

--- src/Lesson11/InsertSort.java
+++ src/Lesson11/InsertSort.java
@@ -1,24 +1,20 @@
 package Lesson11;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class InsertSort {
 
     public static int[] insertSort(int[] array) {
 
-        for (int i = 1; i < array.length; ++i) {
-            if (array[i - 1] > array[i]) {
+        for (int i = 1; i < array.length ; ++i) {
+           if (array[i - 1] > array[i]) {
                 int temp = array[i];
                 int j = i-1;
-                while (j>0 && temp < array[j]){}
-//                while (j > 0 && array[j - 1] > array[j]) {
-//                    int temp = array[j - 1];
-//                    array[j - 1] = array[j];
-//                    array[j] = temp;
-//                    j--;
-//                }
-
+                while (j>=0 && temp < array[j]){
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = temp;
             }
         }
         return array;
